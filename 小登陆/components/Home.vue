de9b0599f5481cfca08800bd83c2551daf714a9c
cuiweiqiang <1074366253@qq.com>
2017-12-02 23:01:57 +0800

    手机号多少

---
+++ 小登陆/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+       <router-link :to="{name:'login'}">登录</router-link>
+        <mt-button type="danger" size="small" @click="exit">退出</mt-button>
+        <router-link :to="{name:'music'}">音乐</router-link>
+        <router-link :to="{name:'music.list'}">音乐列表</router-link>
+       
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            exit(){
+            this.$axios.put('users/cwq',{
+            isLogin:false
+            }).
+            then(res=>{
+            this.$router.push({
+            name:'login'
+            })
+            })
+            }
+        },
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
